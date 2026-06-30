55dacbb7856c5dc45fac65ae62434c88c37db93b
jtiagosantos <tiagosan040@gmail.com>
2026-06-30 09:33:55 -0300

    refactor: refactor register user service

--- app/services/users/register_user_service.rb
+++ app/services/users/register_user_service.rb
@@ -2,9 +2,7 @@ class Users::RegisterUserService
   ALLOWD_STATUS = [ "active", "blocked" ]
 
   def call(params)
-    invalidStatus = params[:status] && !ALLOWD_STATUS.include?(params[:status])
-
-    raise Users::InvalidUserStatusError.new if invalidStatus
+    raise Users::InvalidUserStatusError.new if is_invalid_status?(params[:status])
 
     usersExists = User.exists?(email: params[:email])
 
@@ -16,4 +14,9 @@ class Users::RegisterUserService
       status: params[:status] || "active"
     )
   end
+
+  private
+    def is_invalid_status?(status)
+      status && !ALLOWD_STATUS.include?(status)
+    end
 end
